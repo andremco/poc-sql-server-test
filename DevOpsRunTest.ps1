a2271b264e4c2ff07a7c107d18ff3b62d7d18264
Andre Militao Costa Oliveira <andremco1992@gmail.com>
2021-08-28 22:14:49 -0300

    test in script

--- DevOpsRunTest.ps1
+++ DevOpsRunTest.ps1
@@ -4,4 +4,8 @@ write-host 'AZ Id: ' + $env:AZSUBSCRIPTION
 
 write-host 'ProjectName: ' + $env:PROJECTNAME
 
+$agentIp = (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
+
+write-host $agentip
+
 # . .\RunTest.ps1
