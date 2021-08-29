9834a12fa758f59dca70e6edb76515ae53ca93d8
Andre Militao Costa Oliveira <andremco1992@gmail.com>
2021-08-28 22:32:10 -0300

    settings in script

--- DevOpsRunTest.ps1
+++ DevOpsRunTest.ps1
@@ -7,7 +7,7 @@ write-host 'ProjectName: ' + $env:PROJECTNAME
 # Get ip of agent azure devops for allow in firewall sql server
 $agentIp = (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
 
-az sql server firewall-rule create -g $resGroup -s $nameSqlServerAZ -n myrule --start-ip-address $agentIp --end-ip-address $agentIp
+az sql server firewall-rule create -g $resGroup -s $nameSqlServerAZ -n "myrule$(versionPipe)" --start-ip-address $agentIp --end-ip-address $agentIp
 
 write-host $agentip
 
--- EnvironmentVariable.ps1
+++ EnvironmentVariable.ps1
@@ -6,3 +6,8 @@ if ([string]::IsNullOrEmpty($resGroup)) { throw("Empty var resGroup!") }
 
 $nameSqlServerAZ = $env:NAMESQLSERVERAZ
 if ([string]::IsNullOrEmpty($nameSqlServerAZ)) { throw("Empty var nameSqlServerAZ!") }
+
+# ${env:VERSION}
+
+$versionPipe = $env:VERSION
+if ([string]::IsNullOrEmpty($versionPipe)) { throw("Empty var versionPipe!") }
