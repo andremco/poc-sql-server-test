32fb36dd8fdf23d04a5b5aa8a15a4e1faee989d8
Andre Militao Costa Oliveira <andremco1992@gmail.com>
2021-08-28 22:35:10 -0300

    settings in script

--- DevOpsRunTest.ps1
+++ DevOpsRunTest.ps1
@@ -7,7 +7,9 @@ write-host 'ProjectName: ' + $env:PROJECTNAME
 # Get ip of agent azure devops for allow in firewall sql server
 $agentIp = (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
 
-az sql server firewall-rule create -g $resGroup -s $nameSqlServerAZ -n "myrule$(versionPipe)" --start-ip-address $agentIp --end-ip-address $agentIp
+$nameRuleFirewall = "rule" + $versionPipe
+
+az sql server firewall-rule create -g $resGroup -s $nameSqlServerAZ -n $nameRuleFirewall --start-ip-address $agentIp --end-ip-address $agentIp
 
 write-host $agentip
 
