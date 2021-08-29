d84eadec091bf281f7eb73ecbb6d94ba3e536a91
Andre Militao Costa Oliveira <andremco1992@gmail.com>
2021-08-28 22:26:08 -0300

    allow in firewall sql server

--- DevOpsRunTest.ps1
+++ DevOpsRunTest.ps1
@@ -4,7 +4,10 @@ write-host 'AZ Id: ' + $env:AZSUBSCRIPTION
 
 write-host 'ProjectName: ' + $env:PROJECTNAME
 
+# Get ip of agent azure devops for allow in firewall sql server
 $agentIp = (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
+
+az sql server firewall-rule create -g $resGroup -s $nameSqlServerAZ -n myrule --start-ip-address $agentIp --end-ip-address $agentIp
 
 write-host $agentip
 
--- EnvironmentVariable.ps1
+++ EnvironmentVariable.ps1
@@ -1,2 +1,8 @@
 $connectionString = $env:CONNECTIONSTRING
 if ([string]::IsNullOrEmpty($connectionString)) { throw("Empty var connectionString!") }
+
+$resGroup = $env:RESOURCEGROUP
+if ([string]::IsNullOrEmpty($resGroup)) { throw("Empty var resGroup!") }
+
+$nameSqlServerAZ = $env:NAMESQLSERVERAZ
+if ([string]::IsNullOrEmpty($nameSqlServerAZ)) { throw("Empty var nameSqlServerAZ!") }
