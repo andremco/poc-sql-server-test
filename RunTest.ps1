402be25f5036539ae998bdc15e4d83903a49fc00
Andre Militao Costa Oliveira <andremco1992@gmail.com>
2021-09-07 19:07:50 -0300

    failed tests tsqlt :(

--- RunTest.ps1
+++ RunTest.ps1
@@ -2,13 +2,21 @@
 $sw = [Diagnostics.Stopwatch]::StartNew()
 
 # PrepareServer.sql automatically enables CLR and installs a server certificate that allows the installation of the tSQLt CLR.
-Invoke-Sqlcmd -ServerInstance $nameServer -InputFile $sqlPrepareServer -Verbose
+Invoke-Sqlcmd -ServerInstance $nameServer -InputFile $sqlPrepareServer -Verbose -ErrorAction Stop
 
 # Execute the Example.sql file from the zip file to create an example database (tSQLt_Example) with tSQLt and test cases.
-Invoke-Sqlcmd -ServerInstance $nameServer -InputFile $sqlExample -Verbose
+Invoke-Sqlcmd -ServerInstance $nameServer -InputFile $sqlExample -Verbose -ErrorAction Stop
 
-#  Execute the following script to run all the example tests
-Invoke-Sqlcmd -ServerInstance $nameServer -InputFile $sqlRunAll -Verbose
+try 
+{
+    #  Execute the following script to run all the example tests
+    Invoke-Sqlcmd -ServerInstance $nameServer -InputFile $sqlRunAll -Verbose -ErrorAction Stop
+}
+catch
+{
+    Write-Error "failed tests tsqlt :("
+    Write-Error $_
+}
 
 $sw.Stop()
 write-host "Build time: " $sw.Elapsed.ToString()
