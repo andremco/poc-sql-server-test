ce2015c91670c58108b2a45b9cbed5381964e0be
Andre Militao Costa Oliveira <andremco1992@gmail.com>
2021-09-07 19:24:34 -0300

    Publishing the test results

--- DevOpsRunTest.ps1
+++ DevOpsRunTest.ps1
@@ -8,3 +8,17 @@ sqllocaldb i
 sqllocaldb start MSSQLLocalDB
 
 . .\RunTest.ps1
+
+# Publishing the test results
+$sqlCommand = 'EXEC tSQLt.XmlResultFormatter'
+
+$connection = new-object system.data.SqlClient.SQLConnection($connectionString)
+$command = new-object system.data.sqlclient.sqlcommand($sqlCommand,$connection)
+$connection.Open()
+
+$adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
+$dataset = New-Object System.Data.DataSet
+$adapter.Fill($dataSet) | Out-Null
+
+$connection.Close()
+$dataSet.Tables[0].Rows[0].ItemArray[0] | Out-File "$(System.DefaultWorkingDirectory)/_publish-CI/drop/TEST-tempdb.xml"
